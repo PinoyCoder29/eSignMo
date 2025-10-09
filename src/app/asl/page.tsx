4a414d9e19e3bd418cca04bebe13fdc01d393859
PinoyCoder29 <bicojayvee4@gmail.com>
2025-10-09 13:13:46 +0800

    redisign asl hero page

--- src/app/asl/page.tsx
+++ src/app/asl/page.tsx
@@ -1,19 +1,48 @@
-import { redirect } from "next/navigation";
+import Image from "next/image";
+import image from '../../../public/image/background1.png';
+
 export default function Home() {
-    return(
-        <div>
-            <section className="p-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <h1> A place where everyone understands.</h1>
-                            <p>Learn ASL and FSL, Practice with test, or use real-time speech to sign translation</p>
-                            <button className="btn btn-success px-5">Explore</button>
-                        </div>
-                        <div className="col-md-6"></div>
-                    </div>
-                </div>
-            </section>
+  return (
+    <div>
+      <section className="p-5 p-md-5 py-md-0 position-relative overflow-hidden">
+        <div className="container position-relative">
+          <div className="row">
+              
+            {/* TEXT AREA */}
+            <div className="col-md-7 position-relative z-2 text-md-start mt-md-5">
+              <h1 className="fw-bold">A place where everyone understands.</h1>
+              <p className="fs-5">
+                Learn ASL and FSL, Practice with test, or use real-time
+                speech to sign translation
+              </p>
+              <button className="btn btn-primary px-5">Explore</button>
+            </div>
+
+            {/* IMAGE FOR DESKTOP */}
+            <div className="col-md-5   d-none d-md-block text-center">
+              <Image 
+                src={image} 
+                alt="Sign Language"  
+                width={350} 
+                className="img-fluid"
+              />
+            </div>
+          </div>
+
+          {/* IMAGE FOR MOBILE - ABSOLUTE RIGHT SIDE */}
+          <div 
+            className="d-md-none position-absolute top-50 end-0 translate-middle-y pe-2" 
+            style={{ zIndex: 1 }}
+          >
+            <Image 
+              src={image} 
+              alt="Sign Language mobile" 
+              width={200} 
+              className="img-fluid"
+            />
+          </div>
         </div>
-    )
-}   
+      </section>
+    </div>
+  );
+}
--- src/components/layout/header/FslHeader.tsx
+++ src/components/layout/header/FslHeader.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 export default function FslHeader() {
     return(
         <div>
-           <h1>Fsl Header</h1>
+            <nav className="navbar navbar-expand-md navbar-light bg-light">
+                <div className="container">
+                   <div className="collapse navbar-collapse">
+                      <h1 className="navbar-brand text-success">eSIGN Mo</h1>
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">   
+                            <Link href="" className="nav-link">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="" className="nav-link">Translate</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="" className="nav-link">Learn</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="" className="nav-link">About us</Link>
+                        </li>
+                    </ul>
+                    </div>      
+                </div>
+            </nav>
         </div>
     )
 }
