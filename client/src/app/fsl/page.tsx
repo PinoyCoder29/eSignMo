56a923f5a91bd5f212d6cec81e4ef44cbe02ae1d
PinoyCoder29 <bicojayvee4@gmail.com>
2025-11-14 13:25:33 +0800

    updated fsl pages

--- client/src/app/fsl/page.tsx
+++ client/src/app/fsl/page.tsx
@@ -1,18 +1,21 @@
 export default function Home() {
-    return(
-        <div>
-            <section className="p-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <h1> A place where everyone understands.</h1>
-                            <p>Learn ASL and FSL, Practice with test, or use real-time speech to sign translation</p>
-                            <button className="btn btn-primary px-5">Explore</button>
-                        </div>
-                        <div className="col-md-6"></div>
-                    </div>
-                </div>
-            </section>
+  return (
+    <div>
+      <section className="p-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <h1> A place where everyone understands.</h1>
+              <p>
+                Learn ASL and FSL, Practice with test, or use real-time speech
+                to sign translation
+              </p>
+              <button className="btn btn-primary px-5">Explore</button>
+            </div>
+            <div className="col-md-6"></div>
+          </div>
         </div>
-    )
-}   
+      </section>
+    </div>
+  );
+}
